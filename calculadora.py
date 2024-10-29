4eafc095848ce7ed20850f7f139b61a8799c5c38
Tiago1904-Batista <a13707@esars.pt>
2024-10-29 09:23:59 +0000

    calculadora.py soma e subtração

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,28 @@
 
+def somar (x,y):
+    return x + y
+
+#Função principal
+def calculadora():
+    num1 = int(input("Digite o primeiro número:"))
+    num2 = int(input("Digite o segundo número:"))
+    result = somar  (num1,num2)
+    print("resultado da soma " + str(result))
+
+calculadora()
+
+def surbtrair (x,y):
+  return x - y
+
+#Função principal
+def calculadora():
+  num1 = int(input("Digite o primeiro número:"))
+  num2 = int(input("Digite o segundo número:"))
+  result = subtrair (num1,num2)
+  print("resultado da subtração " + str(result))
+
+calculadora() 
+
+
+
+
