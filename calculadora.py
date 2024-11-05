1437afee4b9f4f9de6cb293ab61790176ae9c7c7
Tiago1904-Batista <a13707@esars.pt>
2024-11-05 09:22:48 +0000

    Funções da soma, divisao, subtraçao e multiplicaçao e o historico.py

--- calculadora.py
+++ calculadora.py
@@ -1,28 +1,35 @@
-
-def somar (x,y):
+# Função de soma
+def soma(x, y):
     return x + y
 
-#Função principal
+# Função de subtração
+def subtração(x, y):
+    return x - y
+
+# Função de multiplicação
+def multiplicação(x, y):
+    return x * y
+
+# Função de divisão
+def divisão(x, y):
+    return x / y
+
+#O histórico começa numa lista vazia
+historico = []
+
 def calculadora():
-    num1 = int(input("Digite o primeiro número:"))
-    num2 = int(input("Digite o segundo número:"))
-    result = somar  (num1,num2)
-    print("resultado da soma " + str(result))
 
-calculadora()
+    def adicionar_ao_historico(operacao):
+        # Se o histórico já tiver 5 elementos, o mais antigo será removido (primeiro)
+        if len(historico) >= 5:
+            historico.pop(0)
+            # Adiciona a nova operação ao final
+        historico.append(operacao)
 
-def surbtrair (x,y):
-  return x - y
+#Apresenta, no histórico a conta toda e não apenas o resultado
+    def mostrar_historico():
+        print("Histórico de operações:")
+        for conta in historico:
+            print(conta)
 
-#Função principal
-def calculadora():
-  num1 = int(input("Digite o primeiro número:"))
-  num2 = int(input("Digite o segundo número:"))
-  result = subtrair (num1,num2)
-  print("resultado da subtração " + str(result))
-
-calculadora() 
-
-
-
-
+   
